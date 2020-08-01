878823b6a268cafa53868beb65fe39a72d151251
MARIO GALEA <mario.s.galea@gmail.com>
2020-08-01 19:20:57 +0200

    added nconf file

--- checkfile.js
+++ checkfile.js
@@ -10,7 +10,6 @@ const fsReadFile = util.promisify(fs.readFile);
 const fsLstat = util.promisify(fs.lstat);
 var eachLine = util.promisify(lineReader.eachLine);
 
-
 const requestListener = async function (req, res) {
     let queryObj = url.parse(req.url, true).query;
     res.setHeader('Content-Type', 'application/json');
@@ -20,20 +19,12 @@ const requestListener = async function (req, res) {
         if (attribute === null) {
             res.end('undefined');
         } else if (attribute === "fileinfo") {
-            const logRecords = await searchFilesInDirectoryAsync(process.cwd(), queryObj[attribute], ".log");
-            let output=[]
-            for (record of logRecords){
-                var JSONrecord = JSON.parse(record);
-                output.push(JSONrecord);
-                //res.pipe(JSONrecord);
-            }
-            console.log(output);
-            res.end(JSON.stringify(output));
+            const logRecords = await searchFilesInDirectoryAsync(process.cwd(), queryObj[attribute], ".log", res);
         }
     }
 }
 
-async function searchFilesInDirectoryAsync(dir, filter, ext) {
+async function searchFilesInDirectoryAsync(dir, filter, ext, response) {
     let logrecord = [];
     const files = await fsReaddir(dir).catch(err => {
         throw new Error(err.message);
@@ -42,19 +33,24 @@ async function searchFilesInDirectoryAsync(dir, filter, ext) {
     const found = await getFilesInDirectoryAsync(dir, ext);
     const regex = new RegExp(filter, 'i');
     console.log(regex);
-
+    response.write("[");
     for (file of found) {
-       await eachLine(file, function (line) {
+        await eachLine(file, function (line) {
             var i = 0;
             if (regex.test(line)) {
-                logrecord.push(line);
-                //console.log(line);
+                var JSONrecord = JSON.parse(line);
+                response.write(JSON.stringify(JSONrecord, ['fileInfo', 'filename', 'hash', 'msg', 'time']));
+                response.write(",");
+                logrecord.push(JSONrecord);
             }
         }).then((err) => {
             if (err) throw err;
-            console.log(file, ": Done");           
+
+            console.log(file, ": Done");
         });
     };
+    //response.write(JSON.stringify(logrecord));
+    response.end("{\"metadata\": \"****END OF SEARCH****\"}]");
     return logrecord;
 }
 
@@ -83,7 +79,6 @@ async function getFilesInDirectoryAsync(dir, ext) {
 
     return files;
 }
-
 
 const server = http.createServer(requestListener);
 server.listen(8080);
--- ftpsrv.js
+++ ftpsrv.js
@@ -1,3 +1,8 @@
+/**
+ * @document MGA EARP FTP Server
+ * @owner Random Systems International
+ */
+const nconf = require('nconf');
 const FtpSrv = require('ftp-srv');
 const bunyan = require('bunyan');
 var fs = require('fs');
@@ -5,119 +10,175 @@ var fsWatcher = require('chokidar');
 var crypto = require('crypto');
 var XMLparser = require('fast-xml-parser');
 
-var key = '412169';
+var fileValid = 0;
+console.log("\033[91m______/\\______ ");
+console.log("\033[4;91mRANDOM SYSTEMS\033[0;39m  \nRandom Systems International. ALL RIGHTS RESERVED 2020.");
+console.log("\033[1;92mMGA FTP SERVER MODULE\033[0;39m\n\n");
 
-var hash = crypto.createHash('sha512', key);
-hash.setEncoding('hex');
+// 2. `process.env`
+// 3. `process.argv`
+nconf.env().argv();
 
+nconf.use('file', {
+    file: 'config.json',
+    logicalSeparator: '.',
+    dir: './',
+    search: true
+});
+
+try {
+    nconf.required(['ftpserver.root_dir', 'ftpserver.port',
+        'ftpserver.url', 'ftpserver.passv_url', 'users'
+    ]);
+} catch (error) {
+    return console.error(error);
+}
+
+// 5. Any default values
+nconf.defaults({
+    sha512_key: "41216M"
+});
+
+console.dir(nconf.get('users'));
+
+/** Set up the application logs */
 const log = bunyan.createLogger({
-    name: "ftpserver",
+    name: "earp",
     streams: [{
             stream: process.stdout,
             level: 'info'
         }, {
-            path: 'ftpserver.log',
+            path: 'earp.log',
             level: 'info'
         },
         {
-            path: 'ftpserver.log',
+            path: 'earp_error.log',
             level: 'error'
         },
     ]
 });
 
+/** Set up the application by loading the app parameters from the config file */
+
 const ftpServer = new FtpSrv({
-    url: 'ftp://127.0.0.1:8880',
-    log: log,
-    pasv_url: '192.168.8.135',
-    pasv_min: 8881,
-    greeting: 'MGA EARP REPORTING SYSTEM. Random Systems Int. All rights reserved 2020',
+    url: nconf.get('ftpserver.url'),
+    pasv_url: nconf.get('ftpserver.passv_url'),
+    pasv_min: nconf.get('ftpserver.port') + 1,
+    greeting: nconf.get('ftpserver.welcomemsg'),
+    anonymous: nconf.get('ftpserver.anonymous'),
     file_format: 'ep',
-    anonymous: 'sillyrabbit',
-    blacklist: ['DELE', 'RNTO']
+    blacklist: ['DELE', 'RNTO'],
+    log: bunyan.createLogger({
+        name: 'ftpsrv',
+        streams: [{
+                stream: process.stdout,
+                level: 'info'
+            }, {
+                path: 'ftpserver.log', //TODO stored in config file
+                level: 'info'
+            },
+            {
+                path: 'ftpserver.log', //TODO stored in config file
+                level: 'error'
+            },
+        ]
+    }, )
 });
 
 var XMLwatcher = fsWatcher.watch(`${process.cwd()}/downloads`, {
     ignored: /(^|[\/\\])\../,
     persistent: true,
     alwaysState: true,
-    usePolling: true,
+    usePolling: false,
     interval: 100,
     binaryInterval: 300,
     awaitWriteFinish: {
-        stabilityThreshold: 2000,
+        stabilityThreshold: 1000,
         pollInterval: 100
-    }
+    },
+    ignorePermissionErrors: false,
 });
 
-XMLwatcher
-    .on('unlink', function (path) {
-        const fileIndex = path.lastIndexOf('\\') + 1;
-        log.info(path.substr(fileIndex), 'has been removed');
-    })
-    .on('add', function (path) {
-        var fileInfo = {};
-        const foundMatch = path.search(/([0-9]{1,4}_20[2-9][0-9]_(0[1-9]|1[0-2])_([0-2][0-9]|3[0-1])_B2(B|C)[A-Z]{5,20}_[0-9]{2}\.xml)$/);
-        if (foundMatch > 0) {
-            var fstream = fs.createReadStream(path);
-            const operIdPattern = path.search(/(_20[2-9][0-9]_(0[1-9]|1[0-2])_([0-2][0-9]|3[0-1])_B2(B|C)[A-Z]{5,20}_[0-9]{2}\.xml)$/);
-            const datePattern = path.search(/(_B2(B|C)[A-Z]{5,20}_[0-9]{2}\.xml)$/);
-            const reportPattern = path.search(/(_[0-9]{2}\.xml)$/);
-            const versionPattern = path.search(/(\.xml)$/);
-            fileInfo.name = path.substring(foundMatch);
-            fileInfo.oper = path.substring(foundMatch, operIdPattern);
-            fileInfo.date = path.substring(operIdPattern + 1, datePattern);
-            fileInfo.type = path.substring(datePattern + 1, reportPattern);
-            fileInfo.vers = parseInt(path.substring(reportPattern + 1, versionPattern));
-            fs.readFile(path, 'utf8', function (err, xmlData) {
-                if (err) {
-                    return log.error(err);
-                }
+
+try {
+    XMLwatcher
+        .on('unlink', function (path) {
+            const fileIndex = path.lastIndexOf('\\') + 1;
+            log.info(path.substr(fileIndex), 'has been removed');
+        })
+        .on('add', function (path) {
+            var fileInfo = {};
+            const foundMatch = path.search(/([0-9]{3,4}_20[2-9][0-9]_(0[1-9]|1[0-2])_([0-2][0-9]|3[0-1])_B2(B|C)[A-Z]{5,20}_V[0-9]{1,3}\.xml)$/i); //TODO stored in config file
+            if (foundMatch > 0) {
+                var fstream = fs.createReadStream(path);
+                const operIdPattern = path.search(/(_20[2-9][0-9]_(0[1-9]|1[0-2])_([0-2][0-9]|3[0-1])_B2(B|C)[A-Z]{5,20}_V[0-9]{1,3}\.xml)$/i); //TODO stored in config file
+                const datePattern = path.search(/(_B2(B|C)[A-Z]{5,20}_V[0-9]{1,3}\.xml)$/i); //TODO stored in config file
+                const reportPattern = path.search(/(V[0-9]{1,3}\.xml)$/i); //TODO stored in config file
+                const versionPattern = path.search(/(\.xml)$/i); //TODO stored in config file
+                fileInfo.filename = path.substring(foundMatch);
+                fileInfo.oper = path.substring(foundMatch, operIdPattern);
+                fileInfo.date = path.substring(operIdPattern + 1, datePattern);
+                fileInfo.type = path.substring(datePattern + 1, reportPattern);
+                fileInfo.vers = parseInt(path.substring(reportPattern + 1, versionPattern));
+                file_id = fs.openSync(path);
+                var xmlData = fs.readFileSync(file_id, 'utf8', function (err, result) {
+                    if (err) {
+                        return log.error(err);
+                    }
+
+                    return result;
+                });
+
                 if (XMLparser.validate(xmlData) === true) { //optional (it'll return an object in case it's not valid)
-                    //var jsonObj = XMLparser.parse(xmlData, options);
-                    //console.log(JSON.stringify(jsonObj, null, 1));
-                    fstream.pipe(hash).on('finish', function () {
-                        fileInfo.hash = hash.read();
+                    var hash512 = crypto.createHash('sha512', nconf.get('sha512_key')).setEncoding('hex'); // this must be placed here as it needs to be declared every time a hash is generated
+                    fstream.pipe(hash512).on('finish', function () {
+                        fileInfo.hash = hash512.read();
                         log.info({
                             fileInfo
                         }, "accepted");
                     });
-
                 } else {
                     const fileIndex = path.lastIndexOf('\\') + 1;
-                    fileInfo.name = path.substr(fileIndex);
+                    fileInfo = path.substr(fileIndex);
                     log.info({
                         fileInfo
-                    }, "invalid");
-                    fs.unlinkSync(path)
+                    }, "invalid_xml_syntax");
+                    fileValid = 1;
                 }
-            });
-        } else {
-            const fileIndex = path.lastIndexOf('\\') + 1;
-            fileInfo.name = path.substr(fileIndex);
-            log.info({
-                fileInfo
-            }, "rejected");
-            fs.unlinkSync(path)
-        }
-    })
-    .on('change', function (path) {
-        log.info(path, 'has been changed');
-    })
-    .on('error', function (error) {
-        log.error('Error happened', error);
-    })
+            } else {
+                const fileIndex = path.lastIndexOf('\\') + 1;
+                fileInfo = path.substr(fileIndex);
+                log.info({
+                    fileInfo
+                }, "rejected");
+                fileValid = 1;
+            }
+            if (fileValid === 1) {
+                console.log(fileValid);
+                fs.unlinkSync(path);
+            }
+        })
+        .on('change', function (path) {
+
+            log.info(path, 'has been changed');
+        })
+        .on('error', function (error) {
+            log.error('Error happened', error);
+        })
+} catch (error) {
+    console.log(error);
+}
 
 ftpServer.on('login', ({
     connection,
     username,
     password
 }, resolve, reject) => {
-    if (username === 'test' && password === 'test' || username === 'anonymous') {
+    if (checkUser(username, password)) { //TODO stored in config file
         resolve({
-            root: `${process.cwd()}/downloads`
+            root: `${process.cwd()}/downloads/${username}`
         });
-        log.info('someone logged in');
+        log.info('user', username, "logged in succesfully.");
     } else {
         log.info('user rejected');
         reject('Bad username or password');
@@ -131,7 +192,14 @@ ftpServer.on('login', ({
 
 });
 
+function checkUser(username, password) {
+    for (user of nconf.get('users')) {
+        if (username === user.userid && password === user.password) return true;
+    }
+    return false;
+}
+
 ftpServer.listen()
     .then((value) => {
-        log.info(value);
+        log.info("Systems Started");
     });
